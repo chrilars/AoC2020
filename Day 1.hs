6cc9696e81327aaf2dadcdbd5b1988e662dfab34
chrilars <31348046+chrilars@users.noreply.github.com>
2020-12-07 20:24:34 +0100

    Dag 1 klart

--- Day 1.hs
+++ Day 1.hs
@@ -1,3 +1,6 @@
-main :: FilePath -> Int
-main x = do
+solve :: [Int] -> Int
+solve x = head [a*b*c | a <- x, b <- x, c <- x, a+b+c == 2020]
+
+main :: IO ()
+main = print . solve . map read . lines =<< readFile "input.txt"
             
